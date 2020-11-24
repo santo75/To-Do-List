92a74a03f123d08a3950ce8ac218c9e7456191c0
Santosh Kumar <santoshkr2911@gmail.com>
2020-11-24 19:49:08 +0530

    Remove button added

--- src/App.js
+++ src/App.js
@@ -1,70 +1,83 @@
-import React from 'react';
-import './App.css';
-import ListItems from './ListItems';
+import React from "react";
+import "./App.css";
+import ListItems from "./ListItems";
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      items:[],
-      currentItem:{
-        text:'',
-        key:''
-      }
-    }
-    this.addItem = this.addItem.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-  }
-  addItem(e){
-    e.preventDefault();
-    const newItem = this.state.currentItem;
-    if(newItem.text !==""){
-      const arr = [...this.state.items, newItem];
-    this.setState({
-      items: arr,
-      currentItem:{
-        text:'',
-        key:''
-      }
-    })
-    }
-  }
-  handleInput(e){
-    this.setState({
-      currentItem:{
-        text: e.target.value,
-        key: Date.now()
-      }
-    })
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: [],
+			currentItem: {
+				text: "",
+				key: "",
+			},
+		};
+		this.addItem = this.addItem.bind(this);
+		this.handleInput = this.handleInput.bind(this);
+	}
+	addItem(e) {
+		e.preventDefault();
+		const newItem = this.state.currentItem;
+		if (newItem.text !== "") {
+			const arr = [...this.state.items, newItem];
+			this.setState({
+				items: arr,
+				currentItem: {
+					text: "",
+					key: "",
+				},
+			});
+		}
+	}
+	handleInput(e) {
+		this.setState({
+			currentItem: {
+				text: e.target.value,
+				key: Date.now(),
+			},
+		});
+	}
 
-  refresh =(e) =>{
-      e.preventDefault();
-      this.setState({
-          items:[]      
-      })
+	handleRemove = key => {
+		const items = this.state.items.filter(i => i.key !== key);
+		this.setState({ items });
+	};
 
-  }
-  
- render(){
-  return (
-    <div className="App">
-      <header>
-        <form id="to-do-form" onSubmit={this.addItem}>
-          <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
-          <button type="submit">Add</button>
-         
-        </form>
-        <p>{this.state.items.text}</p>
-        
-          <ListItems items={this.state.items}/>
-          <button type="submit" id="clr" onClick={this.refresh}>Clear ALL</button>
-        
-      </header>
-    </div>
-  );
- }
+	refresh = e => {
+		e.preventDefault();
+		this.setState({
+			items: [],
+		});
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<header>
+					<form id="to-do-form" onSubmit={this.addItem}>
+						<input
+							type="text"
+							placeholder="Enter task"
+							value={this.state.currentItem.text}
+							onChange={this.handleInput}
+						></input>
+						<button type="submit">Add</button>
+					</form>
+					<p>{this.state.items.text}</p>
+
+					<ListItems items={this.state.items} onRemove={this.handleRemove} />
+					<button
+						type="submit"
+						id="clr"
+						onClick={this.refresh}
+						className="refreshButton"
+					>
+						Clear ALL
+					</button>
+				</header>
+			</div>
+		);
+	}
 }
-
 
 export default App;
--- src/ListItems.js
+++ src/ListItems.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import './ListItems.css';
+import React from "react";
+import "./ListItems.css";
 
-function ListItems(props){
-    const items = props.items;
-    const arr2 = items.map(item =>
-   {
-       return <div className="list" key={item.key}>
-     <p>
-         <input type="text" id={item.key} value={item.text} />
-         <input type="checkbox" className="check" />
+function ListItems(props) {
+	const items = props.items;
+	const arr2 = items.map(item => {
+		if (item.text !== null)
+			return (
+				<div className="list" key={item.key}>
+					<p>
+						<input type="text" id={item.key} value={item.text} />
+						<input type="checkbox" className="check" />
+						<button
+							type="button"
+							className="removeButton"
+							onClick={() => props.onRemove(item.key)}
+						>
+							Remove
+						</button>
+					</p>
+				</div>
+			);
+	});
+	return <div>{arr2}</div>;
+}
 
-     </p>
-     
-    </div>})
-    return <div>
-        
-        {arr2}
-        
-    </div>;
-  }
-
-  export default ListItems;
+export default ListItems;
